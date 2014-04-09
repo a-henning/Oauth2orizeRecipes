1f5113e6736138baf45fc2075946d952dd8aa281
Arthur Henning <h_arthur_12@yahoo.com>
2014-04-09 14:25:28 +0300

    Modified authorization server configuration for CORS.

--- authorization-server/app.js
+++ authorization-server/app.js
@@ -48,6 +48,21 @@ app.use(express.cookieParser());
 app.use(express.urlencoded());
 app.use(express.json());
 
+//CORS Configuration
+app.use('/', function(req, res, next){
+        //res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin,accept,authorization,content-type');
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        } else {
+            next();
+        }
+
+    }
+);
+
 //Session Configuration
 app.use(express.session({
     secret: config.session.secret,
